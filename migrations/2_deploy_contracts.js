351ae4e9f65542c307292ffbf798540ca8356c6a
icohush <ico.hush@gmail.com>
2018-06-30 15:23:08 +0200

    split in 3 calls

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,11 +6,11 @@ module.exports = function (deployer) {
 
 	var maxCap = 698396562013061000000000+10**18;
 				 
-	return deployer.deploy(USHToken).then(function () {
+	return deployer.deploy(USHToken,{gas:1500000}).then(function () {
 		//log the address of the USHToken
 		console.log("USHToken deployed !! address: " + USHToken.address);
 		//deploy the Crowdsale contract
-		return deployer.deploy(Crowdsale, USHToken.address).then(function () {
+		return deployer.deploy(Crowdsale, USHToken.address,{gas:4400000}).then(function () {
 			console.log("Crowdsale deployed address: " + Crowdsale.address);
 			return USHToken.deployed().then(function (token) {
 				return Crowdsale.deployed().then(function (crowdsale) {
@@ -19,12 +19,14 @@ module.exports = function (deployer) {
 					return token.transfer(Crowdsale.address, maxCap).then(function () {
 						token.balanceOf.call(Crowdsale.address).then(function (balance) {
 							console.log("crowdsale balance:",balance.toNumber())
-							return crowdsale.doAlloc().then(function() {
+							return crowdsale.doAlloc({gas:3800000}).then(function() {
 									console.log("alloc 1 done")
-									return crowdsale.doAlloc2().then(function() {
+									return crowdsale.doAlloc2({gas:3800000}).then(function() {
 										console.log("alloc 2 done")
-										return token.balanceOf.call(Crowdsale.address).then(function (balance2) {
-											console.log("crowdsale balance:",balance2.toNumber())
+										return crowdsale.doAlloc3({gas:3800000}).then(function() {
+											return token.balanceOf.call(Crowdsale.address).then(function (balance2) {
+												console.log("crowdsale balance:",balance2.toNumber())
+											});
 										});
 								});
 							});
--- truffle.js
+++ truffle.js
@@ -5,6 +5,13 @@ module.exports = {
       host: "localhost",
       port: 7545,
       network_id: "*" // Match any network id
-    }
+    },
+     ropsten:  {
+     network_id: 3,
+     host: "localhost",
+     port:  8545,
+     gas:   50000,
+     gasPrice: 5000000000
+}
   }
 };
